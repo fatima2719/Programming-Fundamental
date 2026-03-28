4d5e6c263277a439ad31996f518af755ec65a78a
Fatima Amjad <fatimahamjad143@gmail.com>
2026-03-28 19:00:08 +0000

    week6

---
+++ integer.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+main()
+{
+    int numbers[5];
+
+    numbers[0] = 15;
+    numbers[1] = 34;
+    numbers[2] = 44;
+    numbers[3] = 45;
+    numbers[4] = 89;
+
+    cout<<"The 1st element at location number[0] is: " << numbers[0] <<endl;
+    cout<<"The 1st element at location number[1] is: " << numbers[1] <<endl;
+    cout<<"The 1st element at location number[2] is: " << numbers[2] <<endl;
+    cout<<"The 1st element at location number[3] is: " << numbers[3] <<endl;
+    cout<<"The 1st element at location number[4] is: " << numbers[4] <<endl;
+}
